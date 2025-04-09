2d0a8350ae1f225f0f250d64405c06e1ccc600a5
Adnan-Morshed <morshed15-5540@diu.edu.bd>
2025-04-09 22:41:51 +0600

    pblm7

---
+++ lib/problem7.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Online Image Grid',
+      home: ImageGridScreen(),
+    );
+  }
+}
+
+class ImageGridScreen extends StatelessWidget {
+  const ImageGridScreen({super.key});
+
+  // Online image URLs
+  final List<String> imageUrls = const [
+    'https://picsum.photos/200/300?random=1',
+    'https://picsum.photos/200/300?random=2',
+    'https://picsum.photos/200/300?random=3',
+    'https://picsum.photos/200/300?random=4',
+    'https://picsum.photos/200/300?random=5',
+    'https://picsum.photos/200/300?random=6',
+    'https://picsum.photos/200/300?random=7',
+    'https://picsum.photos/200/300?random=8',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isWide = MediaQuery.of(context).size.width > 600;
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Online Image Grid')),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          itemCount: imageUrls.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: isWide ? 4 : 2,
+            crossAxisSpacing: 8,
+            mainAxisSpacing: 8,
+          ),
+          itemBuilder: (context, index) {
+            return ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.network(
+                imageUrls[index],
+                fit: BoxFit.cover,
+                loadingBuilder: (context, child, progress) {
+                  if (progress == null) return child;
+                  return const Center(child: CircularProgressIndicator());
+                },
+                errorBuilder: (context, error, stackTrace) =>
+                    const Center(child: Icon(Icons.error)),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
